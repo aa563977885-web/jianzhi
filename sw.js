0f0bd84282640380c334ab9029669f72f7602953
aa563977885-web <aa563977885@gmail.com>
2026-09-17 00:02:51 +0800

    v2.7 第4-7批：sw fatloss-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fatloss-v3';
+var CACHE='fatloss-v4';
 var ASSETS=['./','./index.html','./manifest.json','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',function(e){
  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
